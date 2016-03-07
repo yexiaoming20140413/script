01cd232eb105e76252534b3f14a5168120e2ead3
xiaoming <yexiaoming@gozap.com>
2016-03-07 16:49:15 +0800

    添加用户注册时间投资时间统计

---
+++ move_msg/check_invest_register_time.py
@@ -0,0 +1,48 @@
+# _*_ coding: utf-8
+#输出投资用户手机号
+import os
+
+import dbConn
+from phone import Phone
+p  = Phone()
+home_type={}
+
+def get_invest_user_phone():
+    conn = dbConn.get_db_conn()
+    cursor = conn.cursor()
+    cursor.execute("select a.userid,b.mobilePhone,b.createTime from (select distinct userid from t_fundrecord where operatetype in (653,726,901 ) and userid is not null) a,t_user b where a.userid=b.id")
+    rows = cursor.fetchall()
+    result=''
+    for item in rows:
+        phone = item['mobilePhone']
+        createTime = item['createTime']
+        userId = item['userid']
+        cursor.execute("select recordTime from t_fundrecord where userId=%s limit 1",[userId])
+        rows1 = cursor.fetchall();
+        for item1 in rows1:
+            recordTime = item1['recordTime']
+        if phone is None:
+            continue
+        if len(phone) != 11:
+            continue
+        result += "手机号："+str(phone)+"   注册时间："+str(createTime)+"   投资时间："+str(recordTime)
+        result += "\r\n"
+    cursor.close()
+    conn.close()
+    return result
+
+
+
+def check_user_by_page():
+    result =''
+    result+=get_invest_user_phone()
+    print "result:" + result
+    filename ='/home/longdai_state_invest_register.txt'
+    file = open(filename,'w')
+    try:
+        file.write(result)
+    finally:
+        file.close()
+    print "check invest register end!"
+
+check_user_by_page()
